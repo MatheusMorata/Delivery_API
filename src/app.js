f08bf1a537b1c280cde35df95903a0902c204561
MatheusMorata <matlucar09@hotmail.com>
2024-08-30 11:00:26 -0300

    FEAT: adicionei cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config({ path: '../.env' })
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const porta = process.env.PORTA
 
+
+app.use(cors())
 app.listen(porta, () => {
     console.log(`Servidor online  http://localhost:${porta}`)
 })
