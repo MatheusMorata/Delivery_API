b147343820b8bd302b64f7d8b91067de8a243345
MatheusMorata <matlucar09@hotmail.com>
2024-08-30 18:43:58 -0300

    FEAT: Criado modelo restaurante

---
+++ src/model/restaurante.js
@@ -0,0 +1,32 @@
+const {Sequelize, DataTypes, Model} = require('sequelize')
+const conexao = require('./database/conexao')
+
+class Restaurante extends Model {}
+
+Restaurante.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nome:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        endereco:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        telefone:{
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        conexao,
+        modelName: 'Restaurante',
+        timestamps: false,
+    },
+)
