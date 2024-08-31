d886705cf8d230f0384ff4c761389a8a49ddec80
MatheusMorata <matlucar09@hotmail.com>
2024-08-30 21:25:10 -0300

    FEAT: Concluindo model Prato

--- src/model/prato.js
+++ src/model/prato.js
@@ -1,3 +1,37 @@
 const {DataTypes, Model} = require('sequelize')
+const conexao = require('./database/conexao')
 
-class Prato extends
+class Prato extends Model {}
+
+Prato.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        IdRestaurante: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Restaurante',
+                key: 'id',
+            },
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        preco: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        }
+    },
+    {
+        conexao,
+        modelName: 'Prato',
+        timestamps: false,
+    },
+)
+
+module.exports = Prato
