e3b01f6995b0e3ed6a951163e8ad789e7ca45815
MatheusMorata <matlucar09@hotmail.com>
2024-08-30 21:47:41 -0300

    FEAT: Criado model cliente

--- src/model/cliente.js
+++ src/model/cliente.js
@@ -1,2 +1,42 @@
 const {DataTypes, Model} = require('sequelize')
 const conexao = require('../database/conexao')
+
+class Cliente extends Model {}
+
+Cliente.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        senha: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        endereco: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        telefone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },{
+        conexao,
+        modelName: 'Cliente',
+        timestamps: false,
+    }
+)
+
+module.exports = Cliente
