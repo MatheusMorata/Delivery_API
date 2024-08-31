66cc4a6624e087067295ad44d402e25b902c58cc
MatheusMorata <matlucar09@hotmail.com>
2024-08-30 21:32:56 -0300

    FEAT: Alteracoes no model pedido

---
+++ src/model/pedido.js
@@ -0,0 +1,20 @@
+const {DataTypes, Model} = require('sequelize')
+const conexao = require('./database/conexao')
+
+class Pedido extends Model {}
+
+Pedido.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+    },
+    {
+        conexao,
+        modelName: 'Pedido',
+        timestamps: false,
+    }
+)
