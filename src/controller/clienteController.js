97ae57a460245f96b886b833ddabc3bdf98f1c28
MatheusMorata <matlucar09@hotmail.com>
2024-09-04 16:18:25 -0300

    FEAT: Criando metodo cadastro no clienteController

---
+++ src/controller/clienteController.js
@@ -0,0 +1,15 @@
+class ClienteController {
+
+    async cadastro(req,res){
+        email = req.body.email
+        
+
+        try{
+
+        }catch(error){
+
+        }
+    }
+}
+
+module.exports = ClienteController;
