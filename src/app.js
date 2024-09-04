00acb567bb059c1f33ec8858b99981875615ae09
MatheusMorata <matlucar09@hotmail.com>
2024-09-04 16:46:27 -0300

    FEAT: Problemas para subir o servidor

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 const conexao = require('./database/conexao')
 const porta = process.env.PORTA
 
-
+app.use(express.json())
 app.use(cors())
 app.use('',rotas)
 app.listen(porta, () => {
