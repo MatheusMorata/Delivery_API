90f87edb77c0ecc505093ea2626f624da1efd650
MatheusMorata <matlucar09@hotmail.com>
2024-09-06 16:03:26 -0300

    FIX: correcao servidor nao subia

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-require('dotenv').config({ path: '../.env' })
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-const rotas = require('./rotas/rotas')
 const app = express()
-const conexao = require('./database/conexao')
-const porta = process.env.PORTA
+const rotas = require('../src/rotas/rotas')
+const conexao = require('../src/database/conexao')
+const porta = process.env.PORTA 
 
 app.use(express.json())
-app.use(cors())
-app.use('',rotas)
+app.use('', rotas)
 app.listen(porta, () => {
-    console.log(`Servidor online  http://localhost:${porta}`)
-})
+  console.log(`Servidor online  http://localhost:${porta}`)
+});
 
-
-( async () => {
-    try{
-        await conexao.sync()
-    }catch(error){
-        console.log("Conexao com banco de dados falhou")
-    }
+(async () => {
+  try{
+    await conexao.sync()
+  }catch(error){
+    console.log('Nao conectou com o banco de dados: ',error)
+  }
 })
