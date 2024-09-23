5b0b2bbe5c0543a752644bb7dd8e8b20445aa758
MatheusMorata <matlucar09@hotmail.com>
2024-09-23 15:04:40 -0300

    FEAT: importando as classes

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -6,4 +6,16 @@ const ClienteController = require('../controller/clienteController')
 const Cliente = new ClienteController()
 rotas.get('/', Cliente.cadastro)
 
+// Todas as rotas destinadas ao pedido
+const PedidoController = require('../controller/pedidoController')
+const Pedido = new PedidoController()
+
+// Todas as rotas destinadas ao prato
+const PratoController = require('../controller/pratoController')
+const Prato = new PratoController()
+
+// Todas as rotas destinadas ao restaurante
+const RestauranteController = require('../controller/restauranteController')
+const Restaurante = new RestauranteController()
+
 module.exports = rotas
